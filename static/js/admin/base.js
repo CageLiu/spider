30c8d15639a2d8f03fc8aab3feb65aed53134299
刘锋 <liufeng@cctvcjw.com>
2011-09-02 11:51:56 +0800

    ...

--- static/js/admin/base.js
+++ static/js/admin/base.js
@@ -1,43 +1,45 @@
 $(document).ready(
 	function(){
 
-		if($.browser.msie){
-			document.write("<div style='font-size:200px;color:#f00;line-height:3em;font-weight:bold;text-align:center'>用IE，要枪毙！</div>");
-		}
-
-
-
-		var $t = $(".menu dt");
-		$("i.hide").eq(0).css("display","block");
-		$t.siblings("dd").eq(0).css("display","block");
-		$t.click(function(){
-			if($(this).next("dd").css("display") == "none"){
-				$(this).next("dd").slideDown("speed");
-				$(this).find("i.show").css("display","none");
-				$(this).find("i.hide").css("display","block");
-			}
-			else{ 
-				$(this).next("dd").slideUp("speed");
-				$(this).find("i.show").css("display","block");
-				$(this).find("i.hide").css("display","none");
-			}
-		});
-		var $m = $(".menu dd li");
-		$m.click(function()
-			{
-			$m.removeClass();
-			$(this).addClass("current");
-		});
-		var $b = $(".toggle_btn");
-		$b.toggle(function(){
-			$("#page").css("margin-left","4px");
-			$(".user").css("width",0);
-		},
-		function(){
-			$("#page").css("margin-left","200px");
-			$(".user").css("width","196px");
-		}
-		);
+/*
+ *        if($.browser.msie){
+ *            document.write("<div style='font-size:200px;color:#f00;line-height:3em;font-weight:bold;text-align:center'>用IE，要枪毙！</div>");
+ *        }
+ *
+ *
+ *
+ *        var $t = $(".menu dt");
+ *        $("i.hide").eq(0).css("display","block");
+ *        $t.siblings("dd").eq(0).css("display","block");
+ *        $t.click(function(){
+ *            if($(this).next("dd").css("display") == "none"){
+ *                $(this).next("dd").slideDown("speed");
+ *                $(this).find("i.show").css("display","none");
+ *                $(this).find("i.hide").css("display","block");
+ *            }
+ *            else{ 
+ *                $(this).next("dd").slideUp("speed");
+ *                $(this).find("i.show").css("display","block");
+ *                $(this).find("i.hide").css("display","none");
+ *            }
+ *        });
+ *        var $m = $(".menu dd li");
+ *        $m.click(function()
+ *            {
+ *            $m.removeClass();
+ *            $(this).addClass("current");
+ *        });
+ *        var $b = $(".toggle_btn");
+ *        $b.toggle(function(){
+ *            $("#page").css("margin-left","4px");
+ *            $(".user").css("width",0);
+ *        },
+ *        function(){
+ *            $("#page").css("margin-left","200px");
+ *            $(".user").css("width","196px");
+ *        }
+ *        );
+ */
 
 	/*
 	 * demo page js start
@@ -54,6 +56,7 @@ $(document).ready(
 	var $switchBtn = $(".switch_workspace span");
 	var workspaceHeight = $workspace.height();
 	var count = 0;
+	var $m = $(".main_menu li");
 
 	
 	$menuBox.css("height",$mainMenu.height() - $logo.outerHeight() + "px");
@@ -89,6 +92,12 @@ $(document).ready(
 		$workspace.stop().animate({"marginLeft":"200px"},{duration:"fast",easing:"linear"});
 		$(this).attr("title","收起菜单");
 	});
+
+	$m.click(function(){
+		$m.removeClass();
+		$(this).addClass("current")
+	})
+
 
 /*
  *    $userBox.find("i").click(function(event){
