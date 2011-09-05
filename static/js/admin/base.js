899c3c826c05e11893efa59ef819af82a241f11f
刘锋 <liufeng@cctvcjw.com>
2011-09-05 17:03:34 +0800

    删除多余文件，合并小文件，修改搜索框位置

--- static/js/admin/base.js
+++ static/js/admin/base.js
@@ -4,47 +4,6 @@ $(document).ready(
 			document.write("<div style='font-size:200px;color:#f00;line-height:3em;font-weight:bold;text-align:center'>用IE，要枪毙！</div>");
 		}
 
-/*
- *        
- *
- *
- *
- *        var $t = $(".menu dt");
- *        $("i.hide").eq(0).css("display","block");
- *        $t.siblings("dd").eq(0).css("display","block");
- *        $t.click(function(){
- *            if($(this).next("dd").css("display") == "none"){
- *                $(this).next("dd").slideDown("speed");
- *                $(this).find("i.show").css("display","none");
- *                $(this).find("i.hide").css("display","block");
- *            }
- *            else{ 
- *                $(this).next("dd").slideUp("speed");
- *                $(this).find("i.show").css("display","block");
- *                $(this).find("i.hide").css("display","none");
- *            }
- *        });
- *        var $m = $(".menu dd li");
- *        $m.click(function()
- *            {
- *            $m.removeClass();
- *            $(this).addClass("current");
- *        });
- *        var $b = $(".toggle_btn");
- *        $b.toggle(function(){
- *            $("#page").css("margin-left","4px");
- *            $(".user").css("width",0);
- *        },
- *        function(){
- *            $("#page").css("margin-left","200px");
- *            $(".user").css("width","196px");
- *        }
- *        );
- */
-
-	/*
-	 * demo page js start
-	 */
 	var $mOnff = $(".menu_onff");
 	var $mainMenu = $(".main_menu");
 	var $menuBox = $(".main_menu dl");
@@ -99,68 +58,6 @@ $(document).ready(
 		$(this).addClass("current")
 	})
 
-
-/*
- *    $userBox.find("i").click(function(event){
- *        $(this).addClass("show");
- *        $userBox.find("ul").slideDown("speed");
- *        event.stopPropagation();
- *    });
- *
- *    $("body").click(function(){
- *        $userBox.find("ul").slideUp("fast");
- *        $userBox.find("i").removeClass("show");
- *    });
- */
-
-/*
- *    $switchBtn.click(function(){
- *        var workspaceHeight = $workspace.height();
- *        if(!$(this).hasClass("current")){
- *        $workspace.eq(0).stop().animate({"margin-top":-$(this).index() * workspaceHeight},{duration:"speed",easing:"swing"});
- *        $workspace.animate({"opacity":0},"fast");
- *        $workspace.eq($(this).index()).animate({"opacity":1},"speed");
- *        count = $(this).index();
- *        }
- *    });
- *
- */
-
-/*快捷键 start*/
-/*
- *    var numkey = ["alt+1","alt+2","alt+3","alt+4"];
- *
- *    $.each(numkey,function(i,n){
- *        $.hotkeys.add(n,function(){
- *            var workspaceHeight = $workspace.height();
- *            $workspace.eq(0).stop().animate({"margin-top":-i * workspaceHeight},{duration:"speed",easing:"swing"});
- *            $workspace.css("visibility","hidden");
- *            $workspace.eq(i).css("visibility","visible");
- *            count = i;
- *        });	
- *    });
- */
-
-/*快捷键 end*/
-
-
-
-
-
-	/*
-	 * demo page js end
-	 */
 	}
 );
 
-function drag_pop(){
-	$(".pop").draggable({
-		disabled:false,
-		handle:".pop_title",
-		containment:'document',
-		opacity:0.7
-	});
-}
-function drop_pop(){
-	$(".pop").draggable({disabled:true})
-}
